87dd8a71220c0003ca3300146b2a1fe79810d2b4
africz <attila.fricz@gmail.com>
2023-12-13 12:11:24 +0100

    in progress

--- frontend/src/components/Countries/FlagList.vue
+++ frontend/src/components/Countries/FlagList.vue
@@ -1,13 +1,86 @@
 <template>
-  <div class="flex justify-center">
+  <div class="container mx-auto">
     <div class="grid grid-cols-3 gap-2">
       <div v-for="(flag, index) in this.flags" :key="index" class="table-active">
         <img v-bind:src="flag.flag" alt="" />
-        <p>{{flag.name }}</p>
+        <p>{{ flag.name }}</p>
       </div>
     </div>
 
-    <div class="pagination-area justify-content-between align-items-center">
+    <div
+      class="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6"
+    >
+      <div class="flex flex-1 justify-between sm:hidden">
+        <a
+          href="#"
+          class="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+          >Previous</a
+        >
+        <a
+          href="#"
+          class="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+          >Next</a
+        >
+      </div>
+      <div class="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
+        <div>
+          <p class="text-sm text-gray-700">
+            Showing
+            <span class="font-medium">{{ this.from }}</span>
+            to
+            <span class="font-medium">{{ this.to }}</span>
+            of
+            <span class="font-medium">{{ this.total }}</span>
+            results
+          </p>
+        </div>
+        <div>
+          <nav class="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
+            <a
+              @click="prevPage"
+              href="#"
+              class="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+            >
+              <span class="sr-only">Previous</span>
+              <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path
+                  fill-rule="evenodd"
+                  d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </a>
+            <div v-for="(link, index) in this.links" :key="index">
+              <a
+                v-if="index > 0 && index < this.max_items"
+                v-bind:class="this.isActive(index)"
+                href="#"
+                @click="linkPage(index)"
+                class="relative z-10 inline-flex items-center px-4 py-2 text-sm font-semibold  focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+                >{{ index }}</a
+              >
+            </div>
+
+            <a
+              @click="nextPage"
+              href="#"
+              class="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+            >
+              <span class="sr-only">Next</span>
+              <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path
+                  fill-rule="evenodd"
+                  d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </a>
+          </nav>
+        </div>
+      </div>
+    </div>
+
+    <!-- <div class="pagination-area justify-content-between align-items-center">
       <p class="text-paragraph">
         Showing
         <span class="fw-bold"> from {{ this.from }} to {{ this.to }}</span>
@@ -35,7 +108,7 @@
           </li>
         </ul>
       </nav>
-    </div>
+    </div> -->
   </div>
 </template>
 <script>
@@ -123,9 +196,9 @@ export default {
     isActive(index) {
       //alert(index+'-'+this.current_page)
       if (index === this.current_page) {
-        return 'page-item active'
+        return 'bg-indigo-600 focus-visible:outline-indigo-600 text-white'
       }
-      return 'page-item'
+      return 'bg-white text-black'
     },
     linkPage(index) {
       this.getData(this.links[index].url)
@@ -137,6 +210,4 @@ export default {
 }
 </script>
 
-<style scoped>
-  
-</style>
+<style scoped></style>
